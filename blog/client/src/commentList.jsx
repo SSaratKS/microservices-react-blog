f4395ea36d9554bfcee8cd971d44136d5117217d
SSaratKS <sarat@s3kdevelopers.com>
2023-02-03 22:04:02 +0000

    1 file modified

--- blog/client/src/commentList.jsx
+++ blog/client/src/commentList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const CommentList = (postId) => {
+const CommentList = ({ postId }) => {
   const [comments, setComments] = useState([]);
 
   const fetchData = async () => {
